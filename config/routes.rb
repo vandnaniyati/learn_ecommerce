8f24375d9ee44ab05815a92e1c4a890c6b14062e
vandna <vandna@niyati.com>
2022-08-05 23:37:15 +0530

    crete Login form and user signin functionality

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
  get "sign_up", to: "registrations#new"
  post "sign_up", to: "registrations#create"
  delete "logout", to: "sessions#destroy"
+
+ get "sign_in", to: "sessions#new"
+ post "sign_in", to: "sessions#create"
  
  resources :products
  resources :brands
